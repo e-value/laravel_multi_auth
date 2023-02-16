0e33d31ae4561bcfa9c91c3dca7a750b3dec43db
e-value <y.miyata506320@gmail.com>
2023-02-16 14:51:28 +0900

    ルーティングパス修正

--- src/multi-auth/routes/web.php
+++ src/multi-auth/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,28 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class,, 'index']'])->name('home');
+
+// Admin ログイン
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('login', [App\Http\Controllers\Admin\Auth\LoginController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('login', [App\Http\Controllers\Admin\Auth\LoginController::class, 'login']);
+});
+
+// Admin ログアウト・ログイン後トップページ
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
+    Route::get('home', [App\Http\Controllers\Admin\HomeController::class,'index'])->name('admin.home');
+    Route::post('logout', [App\Http\Controllers\Admin\Auth\LoginController::class,'logout'])->name('admin.logout');
+});
+
+// Company ログイン
+Route::group(['prefix' => 'company'], function() {
+    Route::get('login', [App\Http\Controllers\Company\Auth\LoginController::class,'showLoginForm'])->name('company.login');
+    Route::post('login', [App\Http\Controllers\Company\Auth\LoginController::class,'login']);
+});
+
+// Company ログアウト・ログイン後トップページ
+Route::group(['prefix' => 'compay', 'middleware' => 'auth:company'], function() {
+    Route::get('home', [App\Http\Controllers\Company\HomeController::class,'index'])->name('company.home');
+    Route::post('logout', [App\Http\Controllers\Company\Auth\LoginController::class,'logout'])->name('company.logout');
+});
